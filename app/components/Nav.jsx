abab84ba676930788b03bfe2e8705956f248d803
kuwar <kuwarsaurav21@gmail.com>
2017-03-09 05:47:30 +0545

    navigation style added

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -11,17 +11,17 @@ var Nav = React.createClass({
               React Time App
             </li>
             <li>
-              <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: "bold"}}>Timer</IndexLink>
+              <IndexLink to="/" activeClassName="active-link" activeStyle={{fontWeight: "bold"}}>Timer</IndexLink>
             </li>
             <li>
-              <Link to="/countdown" activeClassName="active" activeStyle={{fontWeight: "bold"}}>Countdown</Link>
+              <Link to="/countdown" activeClassName="active-link" activeStyle={{fontWeight: "bold"}}>Countdown</Link>
             </li>
           </ul>
         </div>
         <div className="top-bar-right">
           <ul className="menu">
             <li className="menu-text">
-              <a href="https://github.com/kuwar" target="_blank">Saurav Kuwar</a>
+              Created By <a href="https://github.com/kuwar" target="_blank">Saurav Kuwar</a>
             </li>
           </ul>
         </div>
