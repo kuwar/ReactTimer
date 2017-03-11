b898d9c926ccd23a370bf05537f8cfe38b2128c4
kuwar <kuwarsaurav21@gmail.com>
2017-03-11 10:56:46 +0545

    component added

--- app/app.jsx
+++ app/app.jsx
@@ -2,6 +2,8 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hasHistory} = require('react-router');
 var Main = require('Main');
+var Timer = require('Timer');
+var Countdown = require('Countdown');
 
 // Load foundation-sites
 require('style!css!foundation-sites/dist/css/foundation.min.css');
@@ -13,7 +15,9 @@ require('style!css!sass!applicationStyles');
 ReactDOM.render(
 	<Router history={hasHistory}>
 		<Route path="/" component={Main}>
+			<Route path='countdown' component={Countdown}/>
+			<IndexRoute component={Timer}/>
 		</Route>
 	</Router>,
 	document.getElementById('app')
-)
+);
